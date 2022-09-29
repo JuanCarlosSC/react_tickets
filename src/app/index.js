a9e15d457eb8b9bd6d3aa25428cdc36291561c20
Juan Carlos Sanchez Cordova <juancarlos_sanchezcordova@hotmail.com>
2022-09-29 01:34:13 -0700

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// import './index.css';
+import AppUI from './AppUI.js';
+
+
+function App() {
+  return (
+    // <TodoProvider>
+      <AppUI />
+    // </TodoProvider>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './app/index.js';
+// import './index.css';
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// )
+
+
+
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo';
+
+ReactDOM.createRoot(document.querySelector("#root")).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <Demo />
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
