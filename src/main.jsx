208516db2ba25139e40d77f39f4aa42c36e6e464
Juan Carlos Sanchez Cordova <juancarlos_sanchezcordova@hotmail.com>
2022-10-11 01:28:59 -0700

    Add vite js

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
-);
+)
